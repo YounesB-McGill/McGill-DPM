4ea7fc100f9b2e8297e17d713bc5df3d1602bff6
alexbhandari <alexbhandari@gmail.com>
2013-09-23 22:45:21 -0400

    Added OdometryCorrection. LS works. Correction values off but pending fix

--- Odometry/src/OdometryCorrection.java
+++ Odometry/src/OdometryCorrection.java
@@ -1,28 +1,63 @@
 /* 
  * OdometryCorrection.java
  */
+import lejos.nxt.*;
 
 public class OdometryCorrection extends Thread {
 
 	private static final int CORRECTION_PERIOD = 10;
-
+   private static final float d = 3; //distance lightsensor proceeds the center of the robot
 	private Odometer odometer;
-
+   private LightSensor lightsensor = new LightSensor(SensorPort.S4,true);
+   private float countX,countY;
 	// constructor
 	public OdometryCorrection(Odometer odometer) {
 		this.odometer = odometer;
+      countX = 0;
+      countY = 0;
 	}
 
 	// run method (required for Thread)
 	public void run() {
 		int correctionStart, correctionEnd;
-
 		while (true) {
 			correctionStart = (int)System.currentTimeMillis();
-
+         float Ox = odometer.getX();
+         float Oy = odometer.getY();
+         float Ot = odometer.getTheta();
 			// put your correction code here
-
-			// this ensure the odometry correction occurs only once every period
+         if(lightsensor.readValue() < 45)
+         {
+             Sound.twoBeeps();
+             if(Math.abs(Ot)<10) //theta at 0 within threshold (points north)
+             {
+                countX++;
+                float error = Ox+d-30.48f*countX;
+                odometer.setX(Ox+error);
+             }
+             else if(Math.abs(Ot+90)<10) //theta at -90 within threshold (points east)
+             {
+                countY--;
+                float error = Oy-d-30.48f*countY;
+                odometer.setY(Oy+error);
+             }
+             else if(Math.abs(Math.abs(Ot)-180)<10) //theta at 180 within threshold (points south)
+             {
+                countX--;
+                float error = Ox-d+30.48f*countX;
+                odometer.setX(Ox+error);
+             }
+             else if(Math.abs(Ot-90)<10) //theta at 90 within threshold (points west)
+		       {
+                countY++;
+                float error = Oy+d+30.48f*countY;
+                odometer.setY(Oy+error);
+             }
+             else //not supported (something's wrong so do nothing)
+             {
+             }
+         }
+         // this ensure the odometry correction occurs only once every period
 			correctionEnd = (int)System.currentTimeMillis();
 			if (correctionEnd - correctionStart < CORRECTION_PERIOD) {
 				try {
