3973e742751069ec891b0864f17c6f22b01dc03f
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2013-09-26 22:04:11 -0400

    ULTIMATE CODE FOR EPIC DATA ANALYSIS

--- Odometry/doc/Pretty.c
+++ Odometry/doc/Pretty.c
@@ -6,32 +6,37 @@
 
 #define N 10
 
-struct vec2_t {
+struct Vec2_t {
 	double x, y;
 };
 
 /** private (entry point) */
 int main(int argc, char **argv) {
-	double xa[N], ya[N], xr[N], yr[N], xd[N], yd[N];
-	double sum, ssq;
-	double mean, var, stdd;
+	struct Vec2_t a[N], r[N], d[N];
+	struct Vec2_t sum, ssq;
+	struct Vec2_t mean, var, stdd;
 	int i;
-	char buffer[120], buffer2[120], *a, *b;
+	char buffer[120], buffer2[120], *s, *t;
 
 	for(i = 0; i < N; i++) {
-		scanf("%lf\t%lf\t%lf\t%lf\t\n", &xa[i], &ya[i], &xr[i], &yr[i]);
+		scanf("%lf\t%lf\t%lf\t%lf\t\n", &a[i].x, &a[i].y, &r[i].x, &r[i].y);
 	}
 
-	for(i = 0, sum = 0, ssq = 0; i < N; i++) {
-		xd[i] = xa[i] - xr[i];
-		yd[i] = ya[i] - yr[i];
+	for(i = 0, sum.x = sum.y = 0, ssq.x = ssq.y = 0; i < N; i++) {
+		d[i].x = a[i].x - r[i].x;
+		d[i].y = a[i].y - r[i].y;
 		/* compute std dev */
-		sum += xd[i];
-		ssq += xd[i] * xd[i];
+		sum.x += d[i].x;
+		sum.y += d[i].y;
+		ssq.x += d[i].x * d[i].x;
+		ssq.y += d[i].y * d[i].y;
 	}
-	mean   = sum / N;
-	var    = (ssq - sum*sum/N) / (N-1);
-	stdd   = sqrt(var);
+	mean.x = sum.x / N;
+	mean.y = sum.y / N;
+	var.x  = (ssq.x - sum.x*sum.x/N) / (N-1);
+	var.y  = (ssq.y - sum.y*sum.y/N) / (N-1);
+	stdd.x = sqrt(var.x);
+	stdd.y = sqrt(var.y);
 	/*stderr = sqrt((ssq/N - mean*mean) / (double)N);*/
 
 	printf("\\begin{table}[htb]\n");
@@ -40,32 +45,32 @@ int main(int argc, char **argv) {
 	printf("&x (cm)& &y (cm)& &x (cm)& &y (cm)& &x (cm)& &y (cm)& \\\\\n");
 	printf("\\hline\n");
 	for(i = 0; i < N; i++) {
-		snprintf(buffer, sizeof(buffer), "%.1f& %.1f& %.2f& %.2f& %.1f& %.1f \\\\", xa[i], ya[i], xr[i], yr[i], xd[i], yd[i]);
+		snprintf(buffer, sizeof(buffer), "%.1f& %.1f& %.2f& %.2f& %.1f& %.1f \\\\", a[i].x, a[i].y, r[i].x, r[i].y, d[i].x, d[i].y);
 		/* replace '.' by '&.' */
-		for(a = buffer, b = buffer2; a; a++) {
-			if(b == buffer2 + sizeof(buffer2) - 2) {
-				*b = '\0';
+		for(s = buffer, t = buffer2; s; s++) {
+			if(t == buffer2 + sizeof(buffer2) - 2) {
+				*t = '\0';
 				break;
 			}
-			if(a[0] == '.') {
-				b[0] = '&';
-				b[1] = '.';
-				b += 2;
+			if(s[0] == '.') {
+				t[0] = '&';
+				t[1] = '.';
+				t += 2;
 			} else {
-				b[0] = a[0];
-				if(a[0] == '\0') break;
-				b++;
+				t[0] = s[0];
+				if(s[0] == '\0') break;
+				t++;
 			}
 		}
 		printf("%s\n", buffer2);
 	}
 	printf("\\end{tabular}\\end{center}\n");
 	printf("\\caption{Reported error as read by the robot, and real error as read by a ruler and the difference between them for the [un]corrected code.\n");
-	printf("\\caption{The corrected sample standard deviation of the difference of $x$ is %.2f and $y$ is %.2f.}\n", stdd, 0.0);
+	printf("The difference, as $(x, y)$, mean is $(%.2f, %.2f)$, varience is $(%.2f, %.2f)$, and the corrected sample standard deviation is $(%.2f, %.2f)$.}\n", mean.x, mean.y, var.x, var.y, stdd.x, stdd.y);
 	printf("\\label{[a|b]}\n");
 	printf("\\end{table}\n");
 
-	printf("mean %f, varience %f, corrected sample stadard deviation %f\n", mean, var, stdd);
+	//printf("mean %f, varience %f, corrected sample stadard deviation %f\n", mean, var, stdd);
 
 	return EXIT_SUCCESS;
 }
