352901787f3e6444069cf68c56ba23a0d4277389
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2013-09-29 15:59:41 -0400

    much simpler

--- Navigation/src/Lab3.java
+++ Navigation/src/Lab3.java
@@ -2,7 +2,6 @@
  Group 51 -- Alex Bhandari-Young and Neil Edelman */
 
 import lejos.nxt.Button; /* must be linked with lejos */
-import lejos.nxt.ButtonListener;
 import lejos.nxt.LCD;
 
 /* this is the entry point */
@@ -10,20 +9,15 @@ import lejos.nxt.LCD;
 public class Lab3 {
 	/** press any key to stop */
 	public static void main(String args[]) {
-		ButtonListener stop = new PressToStop();
-		Button.ESCAPE.addButtonListener(stop);
-		LCD.clear();
-		LCD.drawString("Press any key to stop.", 0, 0);
-		for( ; ; );
-	}
-}
+		Navigator nav = new Navigator();
 
-/** press to stop button listener; this just stops when you press it */
-class PressToStop implements ButtonListener {
-	public void buttonPressed(Button b) {
-		LCD.drawString("Pressed.", 0, 1);
-	}
-	public void buttonReleased(Button b) {
-		LCD.drawString("Released.", 0, 1);
+		/* nav start */
+		nav.start();
+
+		/* press any key to exit */
+		LCD.clear();
+		LCD.drawString("Press any key.", 0, 0);
+		Button.waitForAnyPress();
+		System.exit(0);
 	}
 }
--- Navigation/src/Navigator.java
+++ Navigation/src/Navigator.java
@@ -1,19 +1,43 @@
 /* Lab 3
  Group 51 -- Alex Bhandari-Young and Neil Edelman */
 
-import lejos.nxt.*; /* must be linked with lejos */
+//import lejos.nxt.*; /* must be linked with lejos */
 
-/* Java */
+/* " . . . robot to an absolute position on the field while avoiding obstacles,
+ by use of the odometer and an ultrasonic sensor */
 
-class Navigator {
+class Navigator extends Thread /*implements Runnable*/ {
 	int var;
 
-	public Navigator(final String ex) {
+	public Navigator() {
 		this.var = 7;
 	}
 
 	public String toString() {
 		String j = "";
 		return j;
+	}
+	
+	/** implements all the navigation */
+	public void run() {
+		for( ; ; ) {
+			try {
+				Thread.sleep(200);
+			} catch (Exception e) {
+				System.out.println("Error: " + e.getMessage());
+			}
+		}
+	}
+	
+	void travelTo(double x, double y) {
+		
+	}
+	
+	void turnTo(double theta) {
+		
+	}
+	
+	boolean isNavigating() {
+		return false;
 	}
 }
