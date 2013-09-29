f30696f58c3a300c7cd00543062e78afd9284b1e
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2013-09-29 14:50:22 -0400

    Started Lab3

---
+++ Navigation/src/Lab3.java
@@ -0,0 +1,23 @@
+/* Lab3.java
+ Group 51 -- Alex Bhandari-Young and Neil Edelman */
+
+import lejos.nxt.*; /* must be linked with lejos */
+
+/* this is the entry point */
+
+public class Lab3 implements Runnable {
+	/** press any key to stop */
+	public static void main(String args[]) {
+		
+	}
+	
+	/** waits for keypress */
+	public void run() {
+		
+	}
+	
+	private {
+		LCD.clear();
+		LCD.drawString("Press any key to stop.", 0, 0);
+	}
+}
---
+++ Navigation/src/Navigator.java
@@ -0,0 +1,19 @@
+/* Lab 3
+ Group 51 -- Alex Bhandari-Young and Neil Edelman */
+
+import lejos.nxt.*; /* must be linked with lejos */
+
+/* Java */
+
+class Navigator {
+	int var;
+
+	public Java(final String ex) {
+		this.var = 7;
+	}
+
+	public String toString() {
+		String j = "";
+		return j;
+	}
+}
