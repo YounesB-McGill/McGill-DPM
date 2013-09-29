287a72cd6936f1802cad2a572f22a730065a2bd8
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2013-09-29 16:40:02 -0400

    slowly

--- Navigation/src/Lab3.java
+++ Navigation/src/Lab3.java
@@ -11,12 +11,20 @@ public class Lab3 {
 	public static void main(String args[]) {
 		Navigator nav = new Navigator();
 
+		/* clear screen */
+		LCD.clear();
+
 		/* nav start */
 		nav.start();
 
+		/* random coordinates as specified in the assignment */
+		nav.travelTo(60, -30);
+		nav.travelTo(30, -30);
+		nav.travelTo(30, -60);
+		nav.travelTo(60,   0);
+
 		/* press any key to exit */
-		LCD.clear();
-		LCD.drawString("Press any key.", 0, 0);
+		LCD.drawString("Press any key.", 0, 1);
 		Button.waitForAnyPress();
 		System.exit(0);
 	}
--- Navigation/src/Navigator.java
+++ Navigation/src/Navigator.java
@@ -1,43 +1,56 @@
 /* Lab 3
  Group 51 -- Alex Bhandari-Young and Neil Edelman */
 
-//import lejos.nxt.*; /* must be linked with lejos */
+/* must be linked with lejos */
+import lejos.nxt.LCD;
+import java.lang.String;
 
 /* " . . . robot to an absolute position on the field while avoiding obstacles,
  by use of the odometer and an ultrasonic sensor */
 
 class Navigator extends Thread /*implements Runnable*/ {
-	int var;
+	boolean isNavigating;
+	String navMessage = "stopped";
 
+	/** constructor */
 	public Navigator() {
-		this.var = 7;
 	}
 
+	/** to string, may be useful */
 	public String toString() {
-		String j = "";
-		return j;
+		return navMessage;
 	}
 	
 	/** implements all the navigation */
 	public void run() {
 		for( ; ; ) {
+			LCD.drawString("Nav: " + navMessage, 0, 0);
 			try {
-				Thread.sleep(200);
+				Thread.sleep(1000);
 			} catch (Exception e) {
 				System.out.println("Error: " + e.getMessage());
 			}
 		}
 	}
 	
+	/** "This method causes the robot to travel to the absolute field
+	 location (x, y). This method should continuously call turnTo(double theta)
+	 and then set the motor speed to forward(straight). This will make sure
+	 that your heading is updated until you reach your exact goal.
+	 (This method will poll the odometer for information)" */
 	void travelTo(double x, double y) {
-		
+		navMessage = "to " + x + ", " + y;
 	}
 	
+	/** "This method causes the robot to turn (on point) to the absolute
+	 heading theta. This method should turn a MINIMAL angle to it's target." */
 	void turnTo(double theta) {
 		
 	}
 	
+	/** "This method returns true if another thread has called travelTo() or
+	 turnTo() and the method has yet to return; false otherwise." */
 	boolean isNavigating() {
-		return false;
+		return isNavigating;
 	}
 }
