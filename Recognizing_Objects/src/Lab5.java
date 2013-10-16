d76f22bfd2ba014eaa00c5ac794fbe1b912358f8
alexbhandari <alexbhandari@gmail.com>
2013-10-15 23:28:15 -0400

    Working on part 2. UltrasonicListener, mean absolute deviance filter.

--- Recognizing_Objects/src/Lab5.java
+++ Recognizing_Objects/src/Lab5.java
@@ -1,20 +1,35 @@
 //Lab 5 -alex
 //see diectory src in this folder (Recognizing_Objects/src/src) it has all the old code, I liked having it separate, but you could move it up into src
 //if you need to, to get it to compile. Cool.
-import lejos.nxt.Buttons;
+import lejos.nxt.Button;
 //import Ulitrasonic and Color sensor
+import lejos.nxt.UltrasonicSensor;
+import lejos.nxt.ColorSensor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.Motor;
+import lejos.util.Timer;
+
 public class Lab5 {
 
 
 
 	public static void main(String args[]) {
+      
+      UltrasonicSensor uSensor = new UltrasonicSensor(SensorPort.S4/*port*/);
+      ColorSensor cSensor = new ColorSensor(SensorPort.S3/*port*/);
+      TwoWheeledRobot robot = new TwoWheeledRobot(Motor.A,Motor.B);
+      Odometer odometer = new Odometer(robot,true);
+      Detection detection = new Detection(odometer,uSensor,cSensor);
+      UltrasonicListener ultrasonicListener = new UltrasonicListener(uSensor);
+      Timer ultrasonicTimer = new Timer(10/*round-up to int 9.375*/,ultrasonicListener); //timeout value in ms
+      //usListener times out every 9ms, assuming 75ms for 8 pings
+      //15 pings in 140.625ms. Distance recalculated on every 8th and 15th ping,
+      //meaning every 75ms then ~66ms repeating after 140ms for the first value.
+      //Get distance calls or returns will need to be timed. Might make getDistance blocking.
 
-      UltrasonicSensor uSensor = new UltrasonicSensor(/*port*/);
-      ColorSensor cSensor = new ColorSensor(/*port*/);
-
-      Odometer odometer = new Odometer();
-      Detection detection = new Detection(odometer,uSensor,cSensor); //still need to create uSensor and cSensor above
-   
+      
+      //start timer
+      ultrasonicTimer.start();
       //spawn thread for exit
    	(new Thread() {
          public void run() {
@@ -25,6 +40,7 @@ public class Lab5 {
    
       //Detection methods called for lab stuff
       //Odometer can be used to move robot here or Dectection
-      //chill swag
+      
+      
    }   
 }
