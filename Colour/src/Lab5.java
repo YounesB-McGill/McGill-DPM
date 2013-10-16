d44d030a9a1a372947ea315480a1ccb76d65add2
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2013-10-16 19:32:17 -0400

    average values over wood,styr and put that in code; commented; used std libraries

--- Colour/src/Lab5.java
+++ Colour/src/Lab5.java
@@ -1,104 +1,132 @@
 /* Lab 5 A, Group 51 -- Alex Bhandari-Young and Neil Edelman */
 
-/* Instead of calculating barycentric coordinates, this method is O(2^|E|),
- but |E| is 1 so it's okay (we have 2 substances.) We normalise our colours to
- make it lighting-independent. Compare with experimetal value for the different
- substances, and pick the closest (Cartesan distance to normalised colour
- values.) */
+/* This method is O(2^{n-1}), but n is 2, so it's okay. We normalise our
+ colours to make it lighting-independent. Compare with experimetal value for
+ the different substances, and pick the closest (Cartesan distance to
+ normalised colour values.) */
+
+/* import javax.vecmath.Vector3f; <- does not have this, write our own :[ */
+/* import java.lang.Comparable; */
 
 import lejos.nxt.LCD;
 import lejos.nxt.Button;
 import lejos.nxt.ColorSensor;
 import lejos.nxt.SensorPort;
-import lejos.nxt.ColorSensor;
-/*import javax.vecmath.Tuple3i;*/ /* for storing the colour -- complians! */
-/*import java.lang.Comparable;*/
 
 /* this is a driver that instantaties a Robot and makes it do stuff */
+
 public class Lab5 {
-	/* experimenatly determined */
-	static final ColourNorm styrofoam = new ColourNorm(32,  38,  26);
-	static final ColourNorm woodblock = new ColourNorm(212, 167, 151);
+
+	/* experimenatly determined over multiple orinatations */
+	static Vector3f styrofoam = new Vector3f(0.569778f, 0.546741f, 0.611144f);
+	static Vector3f woodblock = new Vector3f(0.795851f, 0.438809f, 0.414174f);
 
 	public static void main(String args[]) {
-		ColorSensor cd = new ColorSensor(SensorPort.S3);
+		ColorSensor       cd = new ColorSensor(SensorPort.S3);
 		ColorSensor.Color c;
-		ColourNorm colour;
-		float compStyr, compWood;
+		Vector3f          colour, colourDiff = new Vector3f();
+		float             s, w;
+		int               percent;
+
+		/* the normalisation projects the HSL values onto L = 0.5 so it isn't
+		 affected by natural light (3d -> 2d;) barycentric coordinates are the
+		 square of the normalised values */
+		styrofoam.normalize();
+		woodblock.normalize();
 
 		for( ; ; ) {
+			/* get input */
 			LCD.drawString("hit me", 0,0);
 			if((Button.waitForAnyPress() & Button.ID_ESCAPE) != 0) break;
 			LCD.clear();
+
 			/* store it in useless "Color" then transfer it to useful class */
 			c      = cd.getColor(); /* 0 - 255 */
-			colour = new ColourNorm(c.getRed(), c.getGreen(), c.getBlue());
-			/* compare with styrofoam and wood */
-			compStyr = colour.compare(styrofoam);
-			compWood = colour.compare(woodblock);
+			colour = new Vector3f(c.getRed() / 255f,
+								  c.getGreen() / 255f,
+								  c.getBlue() / 255f);
+			colour.normalize();
+
+			/* compare with styrofoam and wood; I think technically, we should
+			 convert to a quatnion to get the great circle distance, but that's
+			 kind of like measureing a distance between two points taking into
+			 account the curvature of the Earth; it's monotonanic and close */
+			colourDiff.sub(colour, styrofoam);
+			s = colourDiff.lengthSquared();
+			colourDiff.sub(colour, woodblock);
+			w = colourDiff.lengthSquared();
+
 			/* display */
-			LCD.drawString("R "+c.getRed(), 0,1);
-			//  +":"+colour.r+" "+styrofoam.r+"/"+woodblock.r, 0,1);
-			LCD.drawString("G "+c.getGreen(), 0,2);
-			//+":"+colour.g+" "+styrofoam.g+"/"+woodblock.g, 0,2);
-			LCD.drawString("B "+c.getBlue(), 0,3);
-			// +":"+colour.b+" "+styrofoam.b+"/"+woodblock.b, 0,3);
-			/* so it's to sqaure root of the certianty -- no one will notice */
-			LCD.drawString(""+
-						   (int)((1 - Math.min(compStyr, compWood) /
-								  Math.max(compStyr, compWood))*100)+
-						   "% " + ((compStyr < compWood) ? "styrofoam" : "wood"),
-						   0,4);
-			/*LCD.drawString("sf "+styr, 0,5);
-			LCD.drawString("wb "+wood, 0,6);*/
+			LCD.drawString("R "+c.getRed()+"/"+colour.r, 0,1);
+			LCD.drawString("G "+c.getGreen()+"/"+colour.g, 0,2);
+			LCD.drawString("B "+c.getBlue()+"/"+colour.b, 0,3);
+			if(s > w) {
+				percent = (int)(s / (s + w) * 100f);
+				LCD.drawString("I'm " + percent + "% sure it's\na wooden block", 0,4);
+			} else if(w > s) {
+				percent = (int)(w / (s + w) * 100f);
+				LCD.drawString("I'm " + percent + "% sure it's\na styrofoam block", 0,4);
+			} else {
+				LCD.drawString("I'm conflicted", 0,4);
+			}
+			LCD.drawString("s "+s, 0,6);
+			LCD.drawString("w "+w, 0,7);
 		}
 	}
 }
 
-/* package javax.vecmath does not exist; aaaaauuuuuuggghht wtf I just want a
- stupid Vec3i -- fine then I'll write one myself */
+/* package javax.vecmath does not exist; aaaaauuuuuuggghht wtf */
 /* this is a normalised colour on 19:13 bit fixed point
  3*255^2 = 195 075 (18 bit) */
-class ColourNorm /*implements Comparable<ColourNorm>*/ {
+/* no, that's complicated, just use floats */
+class Vector3f /*implements Comparable<ColourNorm> <- only int */ {
 	public float r, g, b;
 
-	/** store 19:13 fixed point normalised value; forget it, store floats */
-	public ColourNorm(final int r, final int g, final int b) {
-		this.r = range(0, r, 255) / 255f;
-		this.g = range(0, g, 255) / 255f;
-		this.b = range(0, b, 255) / 255f;
+	/** empty constructor */
+	public Vector3f() {
+	}
+
+	/** fill constructor */
+	public Vector3f(final float r, final float g, final float b) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
+
+	/** returns the length squared */
+	public float lengthSquared() {
+		return r*r + g*g + b*b;
+	}
+
+	/** returns the length */
+	public float length() {
+		return (float)Math.sqrt(this.lengthSquared());
+	}
+	
+	/** normalises this vector in place */
+	public void normalize() {
+		float d = length();
+
 		/* if it's black as satan, just leave it alone */
-		if(this.r == 0 && this.g == 0 && this.b == 0) return;
-		/* normalise */
-		float oneOverd = 1f / (float)Math.sqrt(this.r*this.r + this.g*this.g + this.b*this.b);
-		this.r *= oneOverd;
-		this.g *= oneOverd;
-		this.b *= oneOverd;
-		/*this.r = (this.r << 13) / d2;
-		this.g = (this.g << 13) / d2;
-		this.b = (this.b << 13) / d2;*/
+		if(d == 0f) return;
+		/* invert */
+		float e = 1f / d;
+		r *= e;
+		g *= e;
+		b *= e;
 	}
 
-	/** this is just there for the constructor */
-	static int range(final int l, final int x, final int h) {
-		if(l > x) {
-			return l;
-		} else if(x > h) {
-			return h;
-		}
-		return x;
+	/** subtract */
+	public void sub(final Vector3f x) {
+		r -= x.r;
+		g -= x.g;
+		b -= x.g;
 	}
-
-	/** compare the values, //this results in 31 max bits
-	 no, floating point lazy */
-	float/*int*/ compare(final ColourNorm x) {
-		float/*int*/ red = r - x.r, green = g - x.g, blue = b - x.b;
-		return red*red + green*green + blue*blue;
+	
+	/** subtract */
+	public void sub(final Vector3f x, final Vector3f y) {
+		r = x.r - y.r;
+		g = x.g - y.g;
+		b = x.g - y.b;
 	}
-
-	/* I don't really see how this would work since we compare it to a known
-	 sample . . .
-	 public int compareTo(final ColourNorm x) {
-		return 0;
-	}*/
 }
