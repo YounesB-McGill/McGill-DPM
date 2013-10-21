ebeae8787592899fbb1b6f26ceaf65fda9170b74
alexbhandari <alexbhandari@gmail.com>
2013-10-21 17:44:34 -0400

    working on' ;

--- Searching/src/Lab5.java
+++ Searching/src/Lab5.java
@@ -4,6 +4,11 @@ import lejos.nxt.LCD;
 import lejos.nxt.Button;
 /* that's a bummer! import java.util.concurrent.Executors; */
 
+private static final SCAN_THRESHOLD = 200;
+private static final DISTANCE_THRESHOLD = 30;
+private static final DESTINATION_X = 30;
+private static final DESTINATION_Y = 90;
+
 /* this is a driver that instantaties a Robot and makes it do stuff */
 class Lab5 {
 	static final int COMMAND_DELAY = 200;
@@ -14,18 +19,46 @@ class Lab5 {
 
 		rt.start();
 
-		/* turning test (haha, get it) */
-		robot.turnTo(45f);
-		System.out.println(""+robot.getStatus());
-		/* wait for it to travel */
+		//find blocks from corner
+      robot.turnConstantlyTo(90f);
+		/* while turning */
+      float targetTheta = -1;
+      float smallestPing = 254;
 		while(robot.getStatus() != Robot.Status.PLOTTING) {
+         int tStart = (int)System.currentTimeMillis();
+         float ping = robot.ping();
+         if(smallestPing > ping) {
+            smallestPing = ping;
+            targetTheta = robot.getPosition().theta;
+         }
+         //pause thread
+         int tEnd = (int)System.currentTimeMillis();
+         int deltaT = tEnd - tStart;
 			try {
-				Thread.sleep(COMMAND_DELAY);
+				Thread.sleep(COMMAND_DELAY - deltaT);
 			} catch (InterruptedException e) {
 				Thread.currentThread().interrupt();
 			}
 		}
-
+      if(targetTheta < SCAN_THRESHOLD) {
+         robot.turnTo(targetTheta);
+         float targetX = robot.getPosition().x + smallestPing*Math.cos(targetTheta);
+         float targetY = robot.getPosition().y + smallestPing*Math.sin(targetTheta);
+         robot.travelTo(targetX,targetY);
+         float distance = robot.ping();
+         while(distance > TRAVEL_THRESHOLD) {
+            distance = robot.ping();
+         }
+         robot.stop();
+         //if styroform go to destination!
+         if(robot.getColor() == robot.getColor().values.styrofoam) {
+            //travel with avoidance
+            robot.travelTo(DESTINATION_X,DESTINATION_Y);
+            while(distance > TRAVEL_THRESHOLD) {
+               
+            }
+         }
+      }
 		System.out.println("Press");
 		Button.waitForAnyPress();
 
