220524a67185b31b6550f930188d2c8125eff674
alexbhandari <alexbhandari@gmail.com>
2013-10-21 18:11:34 -0400

    working on

--- Searching/src/Lab5.java
+++ Searching/src/Lab5.java
@@ -4,18 +4,19 @@ import lejos.nxt.LCD;
 import lejos.nxt.Button;
 /* that's a bummer! import java.util.concurrent.Executors; */
 
-private static final SCAN_THRESHOLD = 200;
-private static final DISTANCE_THRESHOLD = 30;
-private static final DESTINATION_X = 30;
-private static final DESTINATION_Y = 90;
 
 /* this is a driver that instantaties a Robot and makes it do stuff */
 class Lab5 {
 	static final int COMMAND_DELAY = 200;
+   private static final float SCAN_THRESHOLD = 200;
+   private static final float TRAVEL_THRESHOLD = 30;
+   private static final float DESTINATION_X = 30;
+   private static final float DESTINATION_Y = 90;
 
 	public static void main(String args[]) {
 		Robot robot = new Robot();
 		Thread rt = new Thread(robot, Robot.NAME);
+      LCDInfo lcd = new LCDInfo(robot);
 
 		rt.start();
 
@@ -24,6 +25,7 @@ class Lab5 {
 		/* while turning */
       float targetTheta = -1;
       float smallestPing = 254;
+      //while turning get smallest ping value and corresponding theta
 		while(robot.getStatus() != Robot.Status.PLOTTING) {
          int tStart = (int)System.currentTimeMillis();
          float ping = robot.ping();
@@ -40,31 +42,28 @@ class Lab5 {
 				Thread.currentThread().interrupt();
 			}
 		}
+      //if there is a block...
       if(targetTheta < SCAN_THRESHOLD) {
          robot.turnTo(targetTheta);
          float targetX = robot.getPosition().x + smallestPing*Math.cos(targetTheta);
          float targetY = robot.getPosition().y + smallestPing*Math.sin(targetTheta);
          robot.travelTo(targetX,targetY);
          float distance = robot.ping();
-         while(distance > TRAVEL_THRESHOLD) {
+         //move toward it
+         while(distance > TRAVEL_THRESHOLD && robot.getStatus() != Robot.Status.PLOTTING) {
             distance = robot.ping();
          }
          robot.stop();
          //if styroform go to destination!
-         if(robot.getColor() == robot.getColor().values.styrofoam) {
+         if(robot.getColour() == Robot.Colour.Value.STYROFOAM) {
             //travel with avoidance
             robot.travelTo(DESTINATION_X,DESTINATION_Y);
-            while(distance > TRAVEL_THRESHOLD) {
-               
+            while(distance > TRAVEL_THRESHOLD && robot.getStatus() != Robot.Status.PLOTTING) {
+               distance = robot.ping();
             }
          }
       }
-		System.out.println("Press");
-		Button.waitForAnyPress();
 
-		robot.travelTo(10f,0f);
-		System.out.println(""+robot.getStatus());
-		/* wait for it to travel */
 		while(robot.getStatus() != Robot.Status.PLOTTING) {
 			try {
 				Thread.sleep(COMMAND_DELAY);
