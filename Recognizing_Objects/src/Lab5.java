7c022a04fe1f5a3d493ae2cd65cf43cc54110e89
alexbhandari <alexbhandari@gmail.com>
2013-10-22 17:50:56 -0400

    commented

--- Recognizing_Objects/src/Lab5.java
+++ Recognizing_Objects/src/Lab5.java
@@ -1,4 +1,14 @@
 //Lab 5 -- Neil and Alex
+/*
+This file contains the algorithm for scanning the entire field for objects. The robot localizes in the corner of the field at 0,0.
+The "search point" is a point that the robot chooses to scan for blocks. The first search point is at 0,0 and the robot scans ninty degrees,
+starting at theta=0 (parallel with the right hand wall equation: x = -30) to theta=90 (parallel with the left hand wall equation: y=-30).
+The remaining search points start at 30,30 and increaement y by 30 so: (30,30) (30,60) (30,90), and the robot scans 180 degrees. This allows
+it to cover the entire field. When it finds a block it drives near but not into it and uses the color sensor to verify its type. If it is styrofoam
+that robot drives to the destination and exits the loop. If not, the robot backs up and researches for remaining blocks, and then drives to the next
+search point. This allows the entire algorithm to be implemented in the single while loop.
+
+*/
 import lejos.nxt.Button;
 //import Ulitrasonic and Color sensor
 import lejos.nxt.UltrasonicSensor;
@@ -29,13 +39,14 @@ public class Lab5 {
       //Get distance calls or returns will need to be timed. Might make getDistance blocking.
       LCDInfo lcd = new LCDInfo(odometer,ultrasonicListener);
 
-//      Sound.setVolume(50);
-//      USLocalizer usl = new USLocalizer(odometer, new UltrasonicSensor(SensorPort.S4), USLocalizer.LocalizationType./*RISING_EDGE*/FALLING_EDGE);
-//      usl.doLocalization();
-//      odometer.travelTo(-3f,-3f);
-//      odometer.turnTo(45f);
-//      LightLocalizer lsl = new LightLocalizer(odometer, new LightSensor(SensorPort.S1));
-//      lsl.doLocalization();
+      //Localize
+      Sound.setVolume(50);
+      USLocalizer usl = new USLocalizer(odometer, new UltrasonicSensor(SensorPort.S4), USLocalizer.LocalizationType./*RISING_EDGE*/FALLING_EDGE);
+      usl.doLocalization();
+      odometer.travelTo(-3f,-3f);
+      odometer.turnTo(45f);
+      LightLocalizer lsl = new LightLocalizer(odometer, new LightSensor(SensorPort.S1));
+      lsl.doLocalization();
       Sound.setVolume(100);
 
       //start timer
@@ -48,22 +59,26 @@ public class Lab5 {
          }
       }).start();
 
+      //setup variables for searching
       boolean searching = true;
-      float adjust_x = 0;
-      float adjust_y = 0;
-      boolean first = true;
-      while(searching) {
+      float adjust_x = 0; //designates the point on the field to be searched from
+      float adjust_y = 0; //values are (0,0);(30,30);(30,60);(30,90)...etc
+      boolean first = true; //adjust_x is incremented only on the first run to set the second search point
+      while(searching) { //runs until the robot find a blue block
          
-         //update
+         //update x and y
          if(first) {
             adjust_x += 30f;
          }
          adjust_y += 30f;
 
-	   	//find blocks from corner
+	   	//find blocks from search point (starts in corner and progresses along board)
+         //on the first iteration the robot is in the corner and only turns ninty degrees
          odometer.turnTo(90f);
          ultrasonicListener.scan();
-         odometer.turnConstantlyTo(0f);
+         odometer.turnConstantlyTo(0f); //turn constantly disables the p error correction so the robot can ping at a constant speed
+         //if it is not the first iteration, the robot has moved to the center of the field and now turns the full 180 degrees
+         //to search for blocks
          if(!first)
             odometer.turnConstantlyTo(-90f);
          float smallestPing = ultrasonicListener.getSmallestPing(); //stops scan
@@ -72,8 +87,10 @@ public class Lab5 {
          //lcd.setText("TT: " + targetTheta);
          //if there is a block...
          if(smallestPing < SCAN_THRESHOLD) {
+            //when the robot encounters a block, the position of the block is calculated using the measured distance
+            //the robot then move near that position and get a colour reading of the block
             float targetX = odometer.cmGetX() + (smallestPing-2)*(float)Math.cos(Math.toRadians(targetTheta+3));
-            float targetY = odometer.cmGetY() + (smallestPing-2)*(float)Math.sin(Math.toRadians(targetTheta+3)); //small adjustments...
+            float targetY = odometer.cmGetY() + (smallestPing-2)*(float)Math.sin(Math.toRadians(targetTheta+3));
             odometer.travelTo(targetX,targetY); //move near test object
             //if styroform go to destination!
             if(colour.getColourValue() == Colour.Value.STYROFOAM) { //is styrofoam, grab and move
@@ -84,13 +101,14 @@ public class Lab5 {
             }
             else { //is wood move on
                Sound.buzz();
-//               odometer.travelTo(targetX-15,targetY-15); //move back
+               odometer.backup(); //move back
+//             odometer.travelTo(targetX-15,targetY-15); using backup instead
                odometer.travelTo(adjust_x,adjust_y); //move to next scan point
             }
          }
          else
-            Sound.buzz();
-         first = false;
+            Sound.buzz(); //no blocks found
+         first = false; //false after the first iteration of the loop
       }
    }   
 }
